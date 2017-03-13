a09db3c02b6628d7bfac1f1c48c1c6d66d48aa02
Andriy Dubyk <Andriy Dubyk>
2017-03-13 17:47:31 +0200

    created class DTOUser. I removed Registration.h and inserted code from it into mainMenu. Also a few useless files were deleted

--- Lnu/Main.cpp
+++ Lnu/Main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream> 
-#include<windows.h> 
 #include<vector>
 #include<string>
 
@@ -8,27 +7,22 @@ using namespace std;
 
 #include"../Entity/User.h"
 
-#include"../DTO/DtoUser.h"
+#include"../DTO/DTOUser.h"
 
 #include"../Entity/Advertisement.h"
 
-#include"../DTO/DtoAdvertisement.h"
+#include"../DTO/DTOAdvertisement.h"
 
 #include"../AUTH/CorrectLogIn.h"
-
-#include"../AUTH/Registration.h"
 
 #include"../AUTH/ViewAllBeforeLogIn.h"
 
 #include"../AUTH/LogIn.h"
 
-#include"../AUTH/Exit.h"
-
 #include"../AUTH/MainMenu.h"
 
 int main()
 {
-	cout << "\t\t\tHello \n";
 	mainMenu();
 	return 0;
 }
